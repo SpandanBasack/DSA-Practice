424086b48e82cb147da836a79c905c6124dcd61e
SpandanBasack <basackspandan@gmail.com>
2024-07-04 21:33:47 +0530

    Next Greater Element in the array

---
+++ Stack & Queue/Next_Greater.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        stack<long long> s, s_pos;
+        s.push(0);
+        for(int i=1; i<n; i++)
+        {
+            while(!s.empty() && arr[s.top()]<arr[i])
+            {
+                arr[s.top()] = arr[i];
+                s.pop();
+            }
+            s.push(i);
+        }
+        while(!s.empty())
+        {
+            arr[s.top()]=-1;
+            s.pop();
+        }
+        return arr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
